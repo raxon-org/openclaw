5c279d3e8bca12c9714791f5b55c6ce2e6f7394d
Remco van der Velde <remco@universeorange.com>
2026-02-13 15:50:18 +0100

    init

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ echo "composer install -n" >> ~/.bashrc
 echo "cp /Application/vendor/raxon/framework/Bin/Raxon.php /Application/Bin/Raxon.php" >> ~/.bashrc
 echo "php /Application/Bin/Raxon.php bin app" >> ~/.bashrc
 echo "app install raxon/boot -skip=cache-clear" >> ~/.bashrc
+echo "app install raxon/ollama" >> ~/.bashrc
 echo "chown root:root /Application/start.sh" >> ~/.bashrc
 echo "chown root:root /Application/enter.sh" >> ~/.bashrc
 echo "chown root:root /Application/install.dev.sh" >> ~/.bashrc
